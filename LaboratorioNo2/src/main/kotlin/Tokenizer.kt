5d9a6c8f783cabdfb343384a516e5207e8d53658
Jose Ordoñez <joseordonez@MacBook-Pro-de-Jose-2.local>
2024-07-25 22:12:09 -0600

    class Tokenizer

---
+++ LaboratorioNo2/src/main/kotlin/Tokenizer.kt
@@ -0,0 +1,7 @@
+class Tokenizer {
+    private val regex = Regex("""(\d+(\.\d+)?)|([+\-*/()])|(\s+)""")
+
+    fun tokenize(expression: String): List<String> {
+        return regex.findAll(expression).map { it.value }.filter { it.isNotBlank() }.toList()
+    }
+}
